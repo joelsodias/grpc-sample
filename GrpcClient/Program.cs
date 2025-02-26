f39c2a49d95db49dcdc4b62e2dfff743a43862d7
Joelso Dias <joelso.dias@netrin.com.br>
2025-02-25 21:18:41 -0300

    first commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcServer;
+
+class Program
+{
+    static async Task Main()
+    {
+        using var channel = GrpcChannel.ForAddress("http://localhost:5224");
+        var client = new Greeter.GreeterClient(channel);
+
+        var reply = await client.SayHelloAsync(new HelloRequest { Name = "Joelso" });
+        Console.WriteLine($"Resposta do servidor: {reply.Message}");
+    }
+}
